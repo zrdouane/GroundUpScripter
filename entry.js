631291490b09491ba78a0c9eb90c137ee22bb646
zrdouane <ramirdouane@gmail.com>
2024-03-19 06:04:32 +0000

    Entry point of the application.

--- entry.js
+++ entry.js
@@ -1,37 +1,62 @@
-require("dotenv").config()
+/**
+ * @file Entry point of the application.
+ * @module entry
+ */
 
-const express = require("express")
-const expressLayouts = require('express-ejs-layouts')
-const app = express()
+require('dotenv').config();
+
+const express = require('express');
+const expressLayout = require('express-ejs-layouts');
+const methodOverride = require('method-override');
+const cookieParser = require('cookie-parser');
+const session = require('express-session');
+const MongoStore = require('connect-mongo');
+
+const connectDB = require('./server/config/db');
 
 /**
- * ENV PORT
- * Set it in .env
+ * Checks if the current route is active.
+ * @param {string} route - The route to check.
+ * @returns {boolean} - Returns true if the route is active, false otherwise.
  */
-const PORT = process.env.PORT
+const { isActiveRoute } = require('./server/helpers/helper');
+
+const app = express();
 
 /**
- * set static files.
+ * The port number for the server.
+ * @type {number}
  */
-app.use(express.static('public'))
+const PORT = process.env.PORT;
 
-/**
- * Templating Engine
- * use expressLayouts
- * set ejs
- */
-app.use(expressLayouts)
-app.set('layout', './layouts/main')
-app.set('view engine', 'ejs')
+// Connect to DB
+connectDB();
 
-/**
- * routes
- */
-app.use('/', require('./server/routes/main'))
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+app.use(cookieParser());
+app.use(methodOverride('_method'));
 
-/**
- * Listen app
- */
+app.use(session({
+    secret: 'Zack is here',
+    resave: false,
+    saveUninitialized: true,
+    store: MongoStore.create({
+        mongoUrl: process.env.MONGODB_URI
+    }),
+}));
+
+app.use(express.static('public'));
+
+app.use(expressLayout);
+app.set('layout', './layouts/main');
+app.set('view engine', 'ejs');
+
+app.locals.isActiveRoute = isActiveRoute;
+
+app.use('/', require('./server/routes/main'));
+app.use('/', require('./server/routes/admin'));
+
 app.listen(PORT, () => {
-    console.log(`Server is listening at http://localhost:${PORT}`);
+    console.log(`App listening on port ${PORT}`);
 });
